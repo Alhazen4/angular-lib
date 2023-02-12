ae1b2a99c7ac969d9a15e79de2f08de1519809a1
Alhazen4 <cybernonk404@gmail.com>
2023-02-12 15:23:47 +0700

    Commit 2

--- projects/lib-components/src/lib/lib-expand/lib-expand.component.ts
+++ projects/lib-components/src/lib/lib-expand/lib-expand.component.ts
@@ -1,11 +1,9 @@
 import { Component, Input } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'lib-expand',
   templateUrl: './lib-expand.component.html',
   styleUrls: ['./lib-expand.component.scss'],
-  imports: [MatIconModule]
 })
 export class LibExpandComponent {
   @Input() headerTitle: string = 'Title';
